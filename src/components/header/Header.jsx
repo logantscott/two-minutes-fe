b584a0cdfc74ee7f30bffc2b3059a2b9ad339a6b
Logan Scott <logantscott@gmail.com>
2020-08-13 10:23:25 -0600

    styling and some minor refactoring

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,30 +1,26 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './Header.css';
 import { useCurrentUser, useLogout } from '../../hooks/AuthContext';
 
 const Header = () => {
   const currentUser = useCurrentUser();
-  const history = useHistory();
   const logout = useLogout();
 
-  const handleLogout = () => {
-    logout()
-      .then(() => history.push('/'));
-  };
+  const handleLogout = () => logout();
 
   return (
     <nav className={styles.Header}>
       <ul>
         <li><Link to='/'>Home</Link></li>
-        <li>{
+        {
           currentUser ?
             <>
-              <Link to='/dashboard'>Dashboard</Link>
-              <a onClick={handleLogout}>Logout</a>
+              <li><Link to='/dashboard'>Dashboard</Link></li>
+              <li><a onClick={handleLogout}>Logout</a></li>
             </>
             : <Link to='/login'>Signup/Login</Link>
-        }</li>
+        }
       </ul>
     </nav>
   );
